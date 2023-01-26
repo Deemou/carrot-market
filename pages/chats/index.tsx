b74219d7aced9ba5c02e5c08d53da18b581b027d
Deemou <4106243@naver.com>
2023-01-26 22:03:18 +0900

    refactor(components, pages): integrate <Link> tag and <a> tag

--- pages/chats/index.tsx
+++ pages/chats/index.tsx
@@ -8,14 +8,16 @@ const Chats: NextPage = () => {
     <Layout hasTabBar title="채팅">
       <div className="divide-y-[1px] ">
         {new Array(10).fill(1).map((_, i) => (
-          <Link href={`/chats/${i}`} key={Date.now()}>
-            <a className="flex cursor-pointer items-center space-x-3 px-4 py-3">
-              <div className="h-12 w-12 rounded-full bg-slate-300" />
-              <div>
-                <p className="text-gray-700">Steve Jebs</p>
-                <p className="text-sm  text-gray-500">See you tomorrow in the corner at 2pm!</p>
-              </div>
-            </a>
+          <Link
+            href={`/chats/${i}`}
+            key={Date.now()}
+            className="flex cursor-pointer items-center space-x-3 px-4 py-3"
+          >
+            <div className="h-12 w-12 rounded-full bg-slate-300" />
+            <div>
+              <p className="text-gray-700">Steve Jebs</p>
+              <p className="text-sm  text-gray-500">See you tomorrow in the corner at 2pm!</p>
+            </div>
           </Link>
         ))}
       </div>
--- pages/live/index.tsx
+++ pages/live/index.tsx
@@ -9,11 +9,9 @@ const Live: NextPage = () => {
     <Layout hasTabBar title="라이브">
       <div className=" space-y-4 divide-y-[1px]">
         {new Array(7).fill(1).map((_, i) => (
-          <Link key={Date.now()} href={`/live/${i}`}>
-            <a className="block px-4  pt-4">
-              <div className="aspect-video w-full rounded-md bg-slate-300 shadow-sm" />
-              <h1 className="mt-2 text-2xl font-bold text-gray-900">Galaxy S50</h1>
-            </a>
+          <Link key={Date.now()} href={`/live/${i}`} className="block px-4  pt-4">
+            <div className="aspect-video w-full rounded-md bg-slate-300 shadow-sm" />
+            <h1 className="mt-2 text-2xl font-bold text-gray-900">Galaxy S50</h1>
           </Link>
         ))}
         <FloatingButton href="/live/create">
