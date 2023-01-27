13715903185aa619338607e146f037c90d50a0fb
Deemou <4106243@naver.com>
2023-01-27 23:18:43 +0900

    feat: add token logic

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { NextApiRequest, NextApiResponse } from 'next';
 import client from '@libs/server/client';
@@ -5,18 +7,25 @@ import withHandler from '@libs/server/withHandler';
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { phone, email } = req.body;
-  const payload = phone ? { phone: +phone } : { email };
-  const user = await client.user.upsert({
-    where: {
-      ...payload
-    },
-    create: {
-      name: 'Anonymous',
-      ...payload
-    },
-    update: {}
+  const user = phone ? { phone: +phone } : { email };
+  const payload = `${Math.floor(100000 + Math.random() * 900000)}`;
+  const token = await client.token.create({
+    data: {
+      payload,
+      user: {
+        connectOrCreate: {
+          where: {
+            ...user
+          },
+          create: {
+            name: 'Anonymous',
+            ...user
+          }
+        }
+      }
+    }
   });
-  console.log(user);
+  console.log(token);
   return res.status(200).end();
 }
 
