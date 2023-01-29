0fafa732dfef4cc37ee6a9fe001f75505057a388
Deemou <4106243@naver.com>
2023-01-29 20:16:29 +0900

    feat(me): add profile api logic

---
+++ pages/api/users/me.tsx
@@ -0,0 +1,24 @@
+import { withIronSessionApiRoute } from 'iron-session/next';
+import { NextApiRequest, NextApiResponse } from 'next';
+import withHandler, { ResponseType } from '@libs/server/withHandler';
+import client from '@libs/server/client';
+
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>
+) {
+  console.log(req.session.user);
+  const profile = await client.user.findUnique({
+    where: { id: req.session.user?.id }
+  });
+  res.json({
+    ok: true,
+    profile
+  });
+}
+
+export default withIronSessionApiRoute(withHandler('GET', handler), {
+  cookieName: 'carrotsession',
+  password:
+    '9845904809485098594385093840598df;slkgjfdl;gkfsdjg;ldfksjgdsflgjdfklgjdflgjflkgjdgd'
+});
