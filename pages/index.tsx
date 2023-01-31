f33cbf225d86de2b6fef1b06c7b7f48b033cb8a8
Deemou <4106243@naver.com>
2023-02-01 05:06:39 +0900

    feat(pages): replace dummy with product data

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,22 +5,29 @@ import Item from '@components/item';
 import Layout from '@components/layout';
 import useUser from '@libs/client/useUser';
 import Head from 'next/head';
+import useSWR from 'swr';
+import { Product } from '@prisma/client';
+
+interface ProductsResponse {
+  ok: boolean;
+  products: Product[];
+}
 
 const Home: NextPage = () => {
   const { user, isLoading } = useUser();
+  const { data } = useSWR<ProductsResponse>('/api/products');
   return (
     <Layout title="홈" hasTabBar>
       <Head>
         <title>Home</title>
       </Head>
       <div className="flex flex-col space-y-5 divide-y">
-        {new Array(10).fill(1).map((_, i) => (
+        {data?.products?.map((product) => (
           <Item
-            // eslint-disable-next-line react/no-array-index-key
-            key={i}
-            id={i}
-            title="iPhone 14"
-            price={99}
+            id={product.id}
+            key={product.id}
+            title={product.name}
+            price={product.price}
             comments={1}
             hearts={1}
           />
