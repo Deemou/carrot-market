7873a740a3926bec13a210ebe3bf6689b8a90aec
Deemou <4106243@naver.com>
2023-02-04 23:56:50 +0900

    refactor: separate product list into component

--- pages/profile/sold.tsx
+++ pages/profile/sold.tsx
@@ -1,22 +1,13 @@
 import type { NextPage } from 'next';
 import Item from '@components/item';
 import Layout from '@components/layout';
+import ProductList from '@components/product-list';
 
 const Sold: NextPage = () => {
   return (
     <Layout title="판매내역" canGoBack>
       <div className="flex flex-col space-y-5 divide-y  pb-10">
-        {new Array(10).fill(1).map((_, i) => (
-          <Item
-            // eslint-disable-next-line react/no-array-index-key
-            key={i}
-            id={i}
-            title="iPhone 14"
-            price={99}
-            comments={1}
-            hearts={1}
-          />
-        ))}
+        <ProductList kind="sales" />
       </div>
     </Layout>
   );
