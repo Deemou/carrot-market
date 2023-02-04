dbcb67504e6242f42414491b306dbf5e8a6fb632
Deemou <4106243@naver.com>
2023-02-05 00:02:37 +0900

    feat: apply 'useUser' hook by default to all pages except login page

--- libs/client/useUser.ts
+++ libs/client/useUser.ts
@@ -1,16 +1,27 @@
 /* eslint-disable no-void */
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import { User } from '@prisma/client';
 import { useRouter } from 'next/router';
 import { useEffect } from 'react';
 import useSWR from 'swr';
 
+interface ProfileResponse {
+  ok: boolean;
+  profile: User;
+}
+
 export default function useUser() {
-  const { data, error } = useSWR('/api/users/me');
   const router = useRouter();
+  const reqestUrl = '/api/users/me';
+  const loginUrl = '/enter';
+  const profileUrl = '/profile';
+  const { data, error } = useSWR<ProfileResponse>(reqestUrl);
   useEffect(() => {
     if (data && !data.ok) {
-      void router.replace('/enter');
+      void router.replace(loginUrl);
+    }
+    if (data && data.ok && router.pathname === loginUrl) {
+      void router.replace(profileUrl);
     }
   }, [data, router]);
   return { user: data?.profile, isLoading: !data && !error };
