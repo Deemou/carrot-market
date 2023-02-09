43a8c7b016f7df88f6a7502783b1de90688ada03
Deemou <4106243@naver.com>
2023-02-10 07:44:08 +0900

    feat(db): add seeding for db

---
+++ prisma/seed.ts
@@ -0,0 +1,29 @@
+/* eslint-disable @typescript-eslint/require-await */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-misused-promises */
+import { PrismaClient } from '@prisma/client';
+
+const client = new PrismaClient();
+
+async function main() {
+  Array.from({ length: 500 }, (_, i) => i).forEach(async (item) => {
+    await client.stream.create({
+      data: {
+        name: String(item),
+        description: String(item),
+        price: item,
+        user: {
+          connect: {
+            id: 1
+          }
+        }
+      }
+    });
+    console.log(`${item}/500`);
+  });
+}
+
+main()
+  .catch((e) => console.log(e))
+  .finally(() => client.$disconnect());
