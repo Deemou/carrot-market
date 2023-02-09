61ccf6f951703ea5988a12218a2e125ee9b4e1c0
Deemou <4106243@naver.com>
2023-02-09 12:55:05 +0900

    feat(api/users): add api - records

---
+++ pages/api/users/me/records.ts
@@ -0,0 +1,58 @@
+/* eslint-disable no-underscore-dangle */
+import { NextApiRequest, NextApiResponse } from 'next';
+import withHandler, { ResponseType } from '@libs/server/withHandler';
+import client from '@libs/server/client';
+import withApiSession from '@libs/server/withSession';
+import { Kind } from '@prisma/client';
+
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>
+) {
+  const {
+    session: { user },
+    query: { kind }
+  } = req;
+
+  const recordQueries = await client.record.findMany({
+    where: {
+      userId: user?.id,
+      kind: kind as Kind
+    },
+    include: {
+      product: {
+        include: {
+          _count: {
+            select: {
+              records: {
+                where: {
+                  kind: { equals: 'Fav' }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  });
+  const products = recordQueries?.map((record) => {
+    return {
+      ...record,
+      product: {
+        ...record.product,
+        _count: { favs: record.product._count.records }
+      }
+    };
+  });
+  res.json({
+    ok: true,
+    products
+  });
+}
+
+export default withApiSession(
+  withHandler({
+    methods: ['GET'],
+    handler
+  })
+);
