2b430d2f4a7d7bdf77d4ec21f07564740d9d8b14
Deemou <4106243@naver.com>
2023-02-18 19:04:03 +0900

    feat(api/chats): add api - chats id

---
+++ pages/api/chats/[id]/index.ts
@@ -0,0 +1,54 @@
+/* eslint-disable no-underscore-dangle */
+/* eslint-disable consistent-return */
+import { NextApiRequest, NextApiResponse } from 'next';
+import withHandler, { ResponseType } from '@libs/server/withHandler';
+import client from '@libs/server/client';
+import withApiSession from '@libs/server/withSession';
+
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>
+) {
+  const {
+    query: { id, page }
+  } = req;
+  const limit = 10;
+  const chat = await client.chat.findUnique({
+    where: {
+      id: Number(id)
+    },
+    include: {
+      messages: {
+        take: limit,
+        skip: (Number(page) - 1) * limit,
+        orderBy: {
+          createdAt: 'desc'
+        },
+        select: {
+          id: true,
+          message: true,
+          user: {
+            select: {
+              avatar: true,
+              id: true
+            }
+          }
+        }
+      },
+      _count: {
+        select: { messages: true }
+      }
+    }
+  });
+  const lastPage = chat?._count.messages
+    ? Math.ceil(chat._count.messages / limit)
+    : 0;
+  res.json({ ok: true, chat, lastPage });
+}
+
+export default withApiSession(
+  withHandler({
+    methods: ['GET'],
+    handler
+  })
+);
