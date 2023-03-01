43e4f2eeedeb042b82c31647450424febcb86cb0
Deemou <4106243@naver.com>
2023-03-01 11:52:06 +0900

    feat: add api for logout

---
+++ pages/api/users/logout.ts
@@ -0,0 +1,20 @@
+/* eslint-disable @typescript-eslint/await-thenable */
+import { NextApiRequest, NextApiResponse } from 'next';
+import withHandler, { ResponseType } from '@libs/server/withHandler';
+import withApiSession from '@libs/server/withSession';
+
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>
+) {
+  await req.session.destroy();
+
+  res.json({ ok: true });
+}
+
+export default withApiSession(
+  withHandler({
+    methods: ['POST'],
+    handler
+  })
+);
