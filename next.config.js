75a8e20596338d04f7f50cbf183fef7e2cec77a9
Deemou <4106243@naver.com>
2023-03-03 09:19:12 +0900

    chore: replace domains with remotePatterns

--- next.config.js
+++ next.config.js
@@ -3,7 +3,14 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ['firebasestorage.googleapis.com']
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
+        port: '',
+        pathname: '/v0/b/real-carrot.appspot.com/**'
+      }
+    ]
   }
 };
 
