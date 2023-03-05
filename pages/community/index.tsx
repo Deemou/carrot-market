64f30286dc667bf89509516efabf051449c84389
Deemou <4106243@naver.com>
2023-03-05 09:57:00 +0900

    feat(community): add getStaticProps with ISR

--- pages/community/index.tsx
+++ pages/community/index.tsx
@@ -2,11 +2,9 @@
 /* eslint-disable no-underscore-dangle */
 import type { NextPage } from 'next';
 import Link from 'next/link';
-import { useRouter } from 'next/router';
 import useSWR from 'swr';
 import { Post, User } from '@prisma/client';
-import useCoords from '@libs/client/useCoords';
-import { useEffect } from 'react';
+import client from '@libs/server/client';
 import Layout from '@/components/layout';
 import FloatingButton from '@components/floating-button';
 
@@ -19,27 +17,20 @@ interface PostWithUser extends Post {
 }
 
 interface PostsResponse {
-  ok: boolean;
   posts: PostWithUser[];
 }
 
-const Community: NextPage = () => {
-  const router = useRouter();
-  const { latitude, longitude } = useCoords();
-  const { data } = useSWR<PostsResponse>(
-    latitude && longitude
-      ? `/api/posts?latitude=${latitude}&longitude=${longitude}`
-      : null
-  );
-  useEffect(() => {
-    if (data && !data.ok) {
-      void router.replace('/community');
-    }
-  }, [data, router]);
+const Community: NextPage<PostsResponse> = ({ posts }) => {
+  // const { latitude, longitude } = useCoords();
+  // const { data } = useSWR<PostsResponse>(
+  //   latitude && longitude
+  //     ? `/api/posts?latitude=${latitude}&longitude=${longitude}`
+  //     : null
+  // );
   return (
     <Layout seoTitle="Community">
       <div className="space-y-4 divide-y-[2px]">
-        {data?.posts?.map((post) => (
+        {posts?.map((post) => (
           <Link
             key={post.id}
             href={`/community/${post.id}`}
@@ -72,7 +63,7 @@ const Community: NextPage = () => {
                     d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
                   ></path>
                 </svg>
-                <span>궁금해요 {post._count.wonderings}</span>
+                <span>궁금해요 {post._count?.wonderings}</span>
               </span>
               <span className="flex items-center space-x-2 text-sm">
                 <svg
@@ -89,7 +80,7 @@ const Community: NextPage = () => {
                     d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"
                   ></path>
                 </svg>
-                <span>답변 {post._count.answers}</span>
+                <span>답변 {post._count?.answers}</span>
               </span>
             </div>
           </Link>
@@ -114,5 +105,17 @@ const Community: NextPage = () => {
     </Layout>
   );
 };
+
+export async function getStaticProps() {
+  console.log('BUILDING COMM. STATICALLY');
+  const posts = await client.post.findMany({ include: { user: true } });
+  return {
+    props: {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+      posts: JSON.parse(JSON.stringify(posts))
+    },
+    revalidate: 60
+  };
+}
 
 export default Community;
