f2b68a9935768f42bf4d4a5e940ed088b059eef2
Deemou <4106243@naver.com>
2023-04-11 15:10:53 +0900

    fix: issue - logout is not working

--- components/header.tsx
+++ components/header.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable react/button-has-type */
 import useMutation from '@/libs/client/useMutation';
 import useUser from '@/libs/client/useUser';
 import { useEffect } from 'react';
@@ -27,8 +26,13 @@ export default function Header() {
   }, [logoutData]);
   return (
     <div className="fixed top-0 z-10 flex h-20 w-full max-w-xl items-center justify-end space-x-3 bg-black p-2 px-4 font-medium ">
-      <span>{user && `Hello, ${user?.name}!`}</span>
-      <Button onClick={onClick} text="Log out" long={false}></Button>
+      <span>{user && `Hello, ${user.name}!`}</span>
+      <Button
+        onClick={onClick}
+        text="Log out"
+        long={false}
+        type="button"
+      ></Button>
     </div>
   );
 }
