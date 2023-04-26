6c8fb69ef7656ce2f7e09973eca418aabe4e7459
Deemou <4106243@naver.com>
2023-04-27 02:21:54 +0900

    feat: apply SSR to profile/[id]/sold

--- pages/profile/[id]/sold.tsx
+++ pages/profile/[id]/sold.tsx
@@ -1,13 +1,90 @@
-import type { NextPage } from 'next';
-import ProductList from '@components/product-list';
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable no-underscore-dangle */
+import type { NextPage, NextPageContext } from 'next';
 import Layout from '@/components/layout';
+import { withSsrSession } from '@/libs/server/withSession';
+import client from '@/libs/server/client';
+import { Kind } from '@prisma/client';
+import { ProductWithCount } from 'pages';
+import Item from '@/components/item';
 
-const Sold: NextPage = () => {
+interface Record {
+  id: number;
+  product: ProductWithCount;
+}
+
+interface ProductListResponse {
+  ok: boolean;
+  products: Record[];
+}
+
+const Sold: NextPage<ProductListResponse> = ({ products }) => {
   return (
     <Layout seoTitle="Products Sold">
-      <ProductList kind="Sale" />
+      <div className="space-y-5 divide-y pb-10">
+        {products?.map((record) => (
+          <Item
+            id={record.product.id}
+            key={record.id}
+            title={record.product.name}
+            price={record.product.price}
+            image={record.product.image}
+            hearts={record.product._count.favs}
+          />
+        ))}
+      </div>
     </Layout>
   );
 };
+
+interface MyPageContext extends NextPageContext {
+  params: {
+    id: string;
+  };
+}
+
+export const getServerSideProps = withSsrSession(async function (
+  ctx: MyPageContext
+) {
+  const userId = Number(ctx.params?.id);
+  const kind = 'Sale';
+
+  const recordQueries = await client.record.findMany({
+    where: {
+      userId,
+      kind: kind as Kind
+    },
+    include: {
+      product: {
+        include: {
+          _count: {
+            select: {
+              records: {
+                where: {
+                  kind: { equals: 'Sale' }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  });
+  const products = recordQueries?.map((record) => {
+    return {
+      ...record,
+      product: {
+        ...record.product,
+        _count: { favs: record.product._count.records }
+      }
+    };
+  });
+  return {
+    props: {
+      products: JSON.parse(JSON.stringify(products))
+    }
+  };
+});
 
 export default Sold;
