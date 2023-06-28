d35801337d2691ad8dcc0d55b77072e285d6c213
Deemou <4106243@naver.com>
2023-06-28 20:22:37 +0900

    refactor: divide PostSection

--- pages/community/search.tsx
+++ pages/community/search.tsx
@@ -7,6 +7,7 @@ import { Post, User } from '@prisma/client';
 import Layout from '@/components/layout';
 import PaginationBar from '@/components/pagination-bar';
 import SearchBar from '@/components/search-bar';
+import PostSection from '@/components/section/post-section';
 
 interface PostWithUser extends Post {
   user: User;
@@ -38,72 +39,16 @@ const CommunitySearch: NextPage = () => {
   return (
     <Layout seoTitle="CommunitySearch">
       <SearchBar section="community" />
-      <h3 className="mb-4 mt-28">Results for : {q}</h3>
-      <div className="flex flex-col space-y-5 divide-y-[1px]">
-        {data &&
-          data.posts.map((post) => (
-            <Link
-              href={`/community/${post.id}`}
-              key={post.id}
-              className="flex cursor-pointer flex-col items-start pt-4"
-            >
-              <span className="flex items-center rounded-full bg-gray-100 px-2.5 text-gray-800">
-                Question
-              </span>
-              <div className="mt-2">
-                <span className="text-orange-500">Q.</span> {post.question}
-              </div>
-              <div className="mt-5 flex w-full items-center justify-between text-gray-500">
-                <span>{post.user.name}</span>
-                <span>{post.createdAt.toString().slice(0, 10)}</span>
-              </div>
-              <div className="mt-3 flex w-full space-x-5 border-t py-2.5 text-gray-700">
-                <span className="flex items-center space-x-2">
-                  <svg
-                    fill="none"
-                    stroke="currentColor"
-                    viewBox="0 0 24 24"
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="h-4 w-4"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
-                    ></path>
-                  </svg>
-                  <span>궁금해요 {post._count?.wonderings}</span>
-                </span>
-                <span className="flex items-center space-x-2">
-                  <svg
-                    fill="none"
-                    stroke="currentColor"
-                    viewBox="0 0 24 24"
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="h-4 w-4"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"
-                    ></path>
-                  </svg>
-                  <span>답변 {post._count?.answers}</span>
-                </span>
-              </div>
-            </Link>
-          ))}
-        {data && !data.ok && (
-          <div className="mt-10">
-            <h3 className="text-center">No results found</h3>
-          </div>
-        )}
-        {data?.ok && (
-          <PaginationBar currentPage={page} lastPage={data.lastPage} />
-        )}
-      </div>
+      <h3 className="mt-28">Results for : {q}</h3>
+      {data?.ok && <PostSection posts={data.posts}></PostSection>}
+      {data && !data.ok && (
+        <div className="mt-10">
+          <h3 className="text-center">No results found</h3>
+        </div>
+      )}
+      {data?.ok && (
+        <PaginationBar currentPage={page} lastPage={data.lastPage} />
+      )}
     </Layout>
   );
 };
