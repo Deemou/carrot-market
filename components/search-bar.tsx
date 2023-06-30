8b017bfb9ff797743b7cc6257b49f7255974fa39
Deemou <4106243@naver.com>
2023-07-01 07:44:02 +0900

    style: remove style duplication

--- components/search-bar.tsx
+++ components/search-bar.tsx
@@ -8,7 +8,7 @@ interface SearchBarProps {
 
 export default function SearchBar({ section }: SearchBarProps) {
   return (
-    <div className="fixed top-[5rem] z-10 mx-[-1rem] flex h-24 w-full max-w-6xl items-center justify-between  bg-black bg-black px-2">
+    <div className="fixed top-[5rem] z-10 mx-[-1rem] flex h-24 w-full max-w-6xl items-center justify-between bg-black px-2">
       {section === 'products' && <ProductFloatingButton />}
       {section === 'community' && <CommunityFloatingButton />}
       <SearchForm searchUrl={`/${section}/search`} />
