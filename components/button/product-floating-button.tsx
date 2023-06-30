f3eb13b474ed6e920fd902bf107da377737b6993
Deemou <4106243@naver.com>
2023-07-01 06:14:42 +0900

    refactor: divide FloatingButton

---
+++ components/button/product-floating-button.tsx
@@ -0,0 +1,23 @@
+import FloatingButton from '@components/button/floating-button';
+
+export default function ProductFloatingButton() {
+  return (
+    <FloatingButton href="/products/upload">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+        aria-hidden="true"
+        className="h-6 w-6"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="2"
+          d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+        />
+      </svg>
+    </FloatingButton>
+  );
+}
--- components/search-bar.tsx
+++ components/search-bar.tsx
@@ -1,5 +1,6 @@
-import FloatingButton from '@/components/button/floating-button';
 import SearchForm from '@components/form/search-form';
+import ProductFloatingButton from '@components/button/product-floating-button';
+import CommunityFloatingButton from './button/community-floating-button';
 
 interface SearchBarProps {
   section: string;
@@ -8,43 +9,8 @@ interface SearchBarProps {
 export default function SearchBar({ section }: SearchBarProps) {
   return (
     <div className="fixed top-[5rem] z-10 mx-[-1rem] flex h-24 w-full max-w-6xl items-center justify-between  bg-black bg-black px-2">
-      {section === 'products' && (
-        <FloatingButton href="/products/upload">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            aria-hidden="true"
-            className="h-6 w-6"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M12 6v6m0 0v6m0-6h6m-6 0H6"
-            />
-          </svg>
-        </FloatingButton>
-      )}
-      {section === 'community' && (
-        <FloatingButton href="/community/write">
-          <svg
-            fill="none"
-            stroke="currentColor"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-6 w-6"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"
-            ></path>
-          </svg>
-        </FloatingButton>
-      )}
+      {section === 'products' && <ProductFloatingButton />}
+      {section === 'community' && <CommunityFloatingButton />}
       <SearchForm searchUrl={`/${section}/search`} />
     </div>
   );
