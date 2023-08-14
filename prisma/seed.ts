b0bd310fe80d2d60d1614035042da83f5d63d1cb
Deemou <yepchani@gmail.com>
2023-08-14 17:14:40 +0900

    feat: disable seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,24 +3,24 @@ import { PrismaClient } from '@prisma/client';
 const client = new PrismaClient();
 
 async function main() {
-  Array.from({ length: 500 }, (_, i) => i).forEach(async (item) => {
-    await client.stream.create({
-      data: {
-        name: String(item),
-        description: String(item),
-        price: item,
-        user: {
-          connect: {
-            id: 1
-          }
-        },
-        chat: {
-          create: {}
-        }
-      }
-    });
-    console.log(`${item}/500`);
-  });
+  // Array.from({ length: 500 }, (_, i) => i).forEach(async (item) => {
+  //   await client.stream.create({
+  //     data: {
+  //       name: String(item),
+  //       description: String(item),
+  //       price: item,
+  //       user: {
+  //         connect: {
+  //           id: 1
+  //         }
+  //       },
+  //       chat: {
+  //         create: {}
+  //       }
+  //     }
+  //   });
+  //   console.log(`${item}/500`);
+  // });
 }
 
 main()
