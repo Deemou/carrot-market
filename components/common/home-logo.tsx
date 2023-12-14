b070ca5326282b5637c1f7de15798d2564462a94
Deemou <yepchani@gmail.com>
2023-12-14 22:25:14 +0900

    feat: add logo and favicon

--- components/common/home-logo.tsx
+++ components/common/home-logo.tsx
@@ -1,9 +1,11 @@
+import { HOME_URL } from '@/routes';
+import Image from 'next/image';
 import Link from 'next/link';
 
 export default function HomeLogo() {
   return (
-    <Link href="/">
-      <h4 className="font-medium">Carrot Market</h4>
+    <Link href={HOME_URL}>
+      <Image src="/images/carrot-logo.png" alt="Logo" width={32} height={32} />
     </Link>
   );
 }
