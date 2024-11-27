d106e1814ce4ad7119dae6a76c85c093cc2c70ce
Deemou <yepchani@gmail.com>
2024-11-27 19:41:42 +0900

    feat: remove session logic from records

--- pages/api/users/records.ts
+++ pages/api/users/records.ts
@@ -1,22 +1,17 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import withHandler, { ResponseType } from '@libs/server/withHandler';
 import client from '@libs/server/client';
-import { getServerSession } from 'next-auth';
-import { authOptions } from '@api/auth/[...nextauth]';
 import { Kind } from '@prisma/client';
 
 async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseType>
 ) {
-  const session = await getServerSession(req, res, authOptions);
-  if (!session) return res.status(400).json({ ok: false });
-
   const {
     query: { kind, id }
   } = req;
 
-  const userId = Number(id) || Number(session.user.id);
+  const userId = Number(id);
   const page = Number(req.query.page);
   const limit = 10;
   const recordAll = await client.record.findMany({
